9385e43923df8b837be1081be90d9959ba715070
Meruem117 <1572677319@qq.com>
2022-11-16 23:16:03 +0800

    add mysql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common'
+import { Connection } from 'typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UpController } from './up/up.controller'
 import { UpService } from './up/up.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'niit1234',
+      database: 'bili',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true
+    })
+  ],
   controllers: [AppController, UpController],
   providers: [AppService, UpService],
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
